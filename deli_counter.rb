ed910ab52405be8ebc31dfaee022887bc8cc6ea5
Rene <renegarcia1864@gmail.com>
2021-02-04 20:17:37 -0600

    commit tree 7d2f0d922ae8982d87c7c84ec3873be933606269

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,23 +1,24 @@
 # Write your code here.
+require 'pry'
 
-def line(everyone)
-    if everyone.length >= 1
-        nudeli = []
+def line(katz_deli)
+    if katz_deli.length > 0
+        modified_array = []
         counter = 1
-        everyone.each do |name|
-            nudeli.push("#{counter}. #{name}")
+        katz_deli.each do |person|
+            modified_array.push("#{counter}. #{person}")
             counter += 1
         end
-        puts "The line is currently: #{nudeli.join(" ")}"
-    else #if there are people in line 
+        puts "The line is currently: #{modified_array.join(" ")}"
+    else 
         puts "The line is currently empty."
     end
 end
 
-def take_a_number(line, new_person)
-    line.push(new_person)
-    counter = line.length 
-    puts "Welcome, #{new_person}. You are number #{counter} in line."
+def take_a_number(katz_deli, x)
+    katz_deli.push(x)
+    counter = katz_deli.length
+    puts "Welcome, #{x}. You are number #{counter} in line."
 end
 
 def now_serving(line)
@@ -25,10 +26,6 @@ def now_serving(line)
         puts "There is nobody waiting to be served!"
     else
         puts "Currently serving #{line[0]}."
-        line.shift #shift to take out the person infront of the line 
+        line.shift #shift to take out the person infront of the line
     end
 end
-
-
-
-
